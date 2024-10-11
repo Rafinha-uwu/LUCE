a3464bbe92d3abcb65d7628a4abc5e1a62491ea3
Márcio Tavares <mahtavares24@gmail.com>
2024-10-11 15:25:45 +0100

    Player jump (with buffering and coyote time)
    Added Headers in Player Movement script.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,17 +3,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private Rigidbody2D _rb;
+
+    [Header("Movement Settings")]
     [SerializeField] private float _speed;
+    [SerializeField] private float _jumpPower;
+    [SerializeField] private float _jumpBuffer;
+    [SerializeField] private float _coyoteTime;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private Vector2 _groundCheckSize;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
 
     private bool _isFacingRight = true;
+    private bool _isGrounded = false;
     private float _horizontalInput = 0f;
+    private float _jumpBufferCounter = 0f;
+    private float _coyoteTimeCounter = 0f;
 
 
     private void FixedUpdate()
     {
+        HandleCounters();
+        GroundCheck();
         HandleMovement();
         HandleFlip();
+        HandleJump();
+    }
+
+    private void HandleCounters()
+    {
+        _jumpBufferCounter -= Time.fixedDeltaTime;
+        _coyoteTimeCounter -= Time.fixedDeltaTime;
+    }
+
+    private void GroundCheck()
+    {
+        _isGrounded = Physics2D.OverlapBox(_groundCheck.position, _groundCheckSize, 0, _groundLayer);
+        if (_isGrounded) _coyoteTimeCounter = _coyoteTime;
     }
 
     private void HandleMovement()
@@ -33,8 +62,22 @@ public class PlayerMovement : MonoBehaviour
         transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 
+    private void HandleJump()
+    {
+        if (_jumpBufferCounter <= 0 || _coyoteTimeCounter <= 0) return;
+
+        _rb.velocity = new Vector2(_rb.velocity.x, _jumpPower);
+        _jumpBufferCounter = 0;
+        _coyoteTimeCounter = 0;
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         _horizontalInput = context.ReadValue<Vector2>().x;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed) _jumpBufferCounter = _jumpBuffer;
     }
 }
