617e21ffb554b769ac9d4ba328f1972682421765
Márcio Tavares <mahtavares24@gmail.com>
2024-10-26 18:10:52 +0100

    Added new input action, layer & scene.
    Extracted GroundCheck and InputHandler MonoBehaviours from PlayerMovement

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class GroundCheck : MonoBehaviour
+{
+    [SerializeField] private Vector2 _groundCheckSize;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
+
+    public bool IsGrounded { get; private set; }
+
+
+    private void FixedUpdate()
+    {
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(_groundCheck.position, _groundCheckSize, 0, _groundLayer);
+        IsGrounded = colliders.Any(collider => collider.gameObject != gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/InputHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private float _jumpBuffer;
+    
+    public float HorizontalInput { get; private set; }
+    public float JumpBufferCounter { get; private set; }
+    public bool PushPullAction { get; private set; }
+
+
+    public void ClearJumpBuffer()
+    {
+        JumpBufferCounter = 0f;
+    }
+
+    private void FixedUpdate()
+    {
+        if (JumpBufferCounter > 0) JumpBufferCounter -= Time.fixedDeltaTime;
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        HorizontalInput = context.ReadValue<Vector2>().x;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed) JumpBufferCounter = _jumpBuffer;
+    }
+
+    public void OnPushPull(InputAction.CallbackContext context)
+    {
+        PushPullAction = context.ReadValueAsButton();
+    }
+}
