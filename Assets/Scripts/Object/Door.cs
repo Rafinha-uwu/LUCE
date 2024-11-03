7dd0254d2c577f095b8c830c46318e346e1a4017
Márcio Tavares <mahtavares24@gmail.com>
2024-11-03 14:35:02 +0000

    Created Door and some switch scripts
    Door is subclass of SwitchWithRequirements.
    Lever and PressurePlate are subclasses of SwitchObject.
    Button is subclass of Lever.

---
+++ Assets/Scripts/Object/Door.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class Door : SwitchWithRequirements
+{
+    private BoxCollider2D _collider;
+    private ShadowCaster2D _shadowCaster;
+    private SpriteRenderer _renderer;
+
+    protected override void Awake()
+    {
+        _collider = GetComponent<BoxCollider2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+        _shadowCaster = GetComponent<ShadowCaster2D>();
+
+        OnStateChange += OnDoorStateChange;
+        base.Awake();
+    }
+
+    protected override void OnDestroy()
+    {
+        OnStateChange -= OnDoorStateChange;
+        base.OnDestroy();
+    }
+
+
+    private void OnDoorStateChange(SwitchObject switchObject, bool isOn)
+    {
+        bool isClosed = !isOn;
+
+        _collider.enabled = isClosed;
+        _renderer.enabled = isClosed;
+        if (_shadowCaster != null) _shadowCaster.enabled = isClosed;
+    }
+}
---
+++ Assets/Scripts/Object/PressurePlate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PressurePlate : SwitchObject
+{
+    private readonly List<GameObject> _objectsOnPlate = new();
+
+
+    protected override void Start()
+    {
+        IsOn = false;
+        base.Start();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_objectsOnPlate.Contains(collision.gameObject)) return;
+
+        _objectsOnPlate.Add(collision.gameObject);
+        TurnOn();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _objectsOnPlate.Remove(collision.gameObject);
+        if (_objectsOnPlate.Count == 0) TurnOff();
+    }
+}
