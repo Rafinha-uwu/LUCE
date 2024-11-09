a0a511f93a02328b4807014df2c5f86c229f242a
Márcio Tavares <mahtavares24@gmail.com>
2024-11-09 23:12:12 +0000

    Created PlayerHoldItem and HoldableItem scripts

---
+++ Assets/Scripts/Interactions/HoldableItem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class HoldableItem : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private Transform _lastParent;
+
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void StartHold(Transform holdPosition)
+    {
+        _lastParent = transform.parent;
+        transform.SetParent(holdPosition);
+        transform.position = holdPosition.position;
+
+        _rigidbody2D.simulated = false;
+    }
+
+    public void StopHold()
+    {
+        transform.SetParent(_lastParent);
+
+        _rigidbody2D.simulated = true;
+    }
+}
