19e05d8db24c6a9f2bc48b215232426b6a5b036a
Rafinha <rafael.rocha.matos@gmail.com>
2024-11-30 15:45:56 +0000

    Polaroids

---
+++ Assets/Scripts/Interactions/Polaroid.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class Polaroid : MonoBehaviour
+{
+
+    public GameObject Player;
+    public GameObject Colect;
+    public GameObject Black;
+
+    private CountColect count;
+    public bool On;
+    public bool Narrative1 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = Colect.GetComponent<CountColect>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (On)
+        {
+            if (transform.IsChildOf(Player.transform))
+            {
+                if (Narrative1)
+                {
+                    Colect.GetComponent<Animator>().SetBool("Nar1", true);
+                    Black.GetComponent<Animator>().SetBool("Dark", true);
+                    StartCoroutine(Die1());
+                }
+                else
+                {
+                    Colect.GetComponent<Animator>().SetBool("Found", true);
+                    Black.GetComponent<Animator>().SetBool("Dark", true);
+
+                    StartCoroutine(Die());
+                }
+            }
+        }
+    }
+
+
+    public IEnumerator Die()
+    {
+        On = false;
+        yield return new WaitForSeconds(1f);
+        Colect.GetComponent<Animator>().SetBool("Found", false);
+        yield return new WaitForSeconds(2.5f);
+        count.nColect++;
+        yield return new WaitForSeconds(1f);
+        Black.GetComponent<Animator>().SetBool("Dark", false);
+
+        Destroy(this.gameObject);
+    }
+    public IEnumerator Die1()
+    {
+        On = false;
+        yield return new WaitForSeconds(1f);
+        Colect.GetComponent<Animator>().SetBool("Nar1", false);
+        yield return new WaitForSeconds(2.5f);
+        count.nColect++;
+        yield return new WaitForSeconds(20f);
+        Black.GetComponent<Animator>().SetBool("Dark", false);
+
+        Destroy(this.gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/Other/CountColect.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using TMPro;
+
+public class CountColect : MonoBehaviour
+{
+
+    public float nColect = 0;
+
+    private TextMeshProUGUI tmpText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmpText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+       tmpText.text = nColect + "/6";
+
+    }
+}
