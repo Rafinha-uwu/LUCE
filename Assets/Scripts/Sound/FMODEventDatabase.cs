c61a6f279b429230ed73cc0109c8120e6473d8df
Márcio Tavares <marciotavares@ua.pt>
2025-01-13 14:31:35 +0000

    Added events to the FMODEventDatabase script

--- Assets/Scripts/Sound/FMODEventDatabase.cs
+++ Assets/Scripts/Sound/FMODEventDatabase.cs
@@ -5,4 +5,31 @@ using FMODUnity;
 public class FMODEventDatabase : ScriptableObject
 {
     [field: SerializeField] public EventReference EventForTesting { get; private set; }
+
+    // Game + Start Menu
+    [field: SerializeField] public EventReference GameBGM { get; private set; }
+    [field: SerializeField] public EventReference GameAmbience { get; private set; }
+    [field: SerializeField] public EventReference StartMenuBGM { get; private set; }
+
+    // Player
+    [field: SerializeField] public EventReference PlayerMove { get; private set; }
+    [field: SerializeField] public EventReference PlayerJump { get; private set; }
+    [field: SerializeField] public EventReference PlayerGroundHit { get; private set; }
+    [field: SerializeField] public EventReference PlayerDeath { get; private set; }
+
+    // Objects
+    [field: SerializeField] public EventReference ObjectMove { get; private set; }
+    [field: SerializeField] public EventReference ObjectGroundHit { get; private set; }
+
+    // Interactions
+    [field: SerializeField] public EventReference Lever { get; private set; }
+    [field: SerializeField] public EventReference Button { get; private set; }
+    [field: SerializeField] public EventReference PressurePlate { get; private set; }
+    [field: SerializeField] public EventReference Elevator { get; private set; }
+    [field: SerializeField] public EventReference Door { get; private set; }
+    
+    // Lights
+    [field: SerializeField] public EventReference Light { get; private set; }
+    [field: SerializeField] public EventReference MovingLight { get; private set; }
+
 }
