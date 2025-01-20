1a4b0d0ccc5d1d65af5d61e405213c0e2dffcf40
Márcio Tavares <marciotavares@ua.pt>
2025-01-20 23:39:36 +0000

    Logic to LightMove sounds + AnimationBehaviours
    Moved ElevatorAnimationBehaviour to new directory.
    Created LightMoveAnimationBehaviour to call methods on state enter.

--- Assets/Scripts/Interactions/LightMove.cs
+++ Assets/Scripts/Interactions/LightMove.cs
@@ -1,3 +1,4 @@
+using FMODUnity;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
@@ -7,6 +8,12 @@ public class LightMove : MonoBehaviour
     private static readonly string ANIMATOR_PARAMETER = "Move";
 
     private Animator _animator;
+
+    private static readonly string EVENT_PARAMETER_STATE = "LightState";
+    private StudioEventEmitter _lightMoveSound;
+    [SerializeField] private float _soundMinDistance = 0f;
+    [SerializeField] private float _soundMaxDistance = 10f;
+
 
     private void Awake()
     {
@@ -30,4 +37,31 @@ public class LightMove : MonoBehaviour
     }
 
 
+    private void Start()
+    {
+        _lightMoveSound = GetLightMoveSound();
+    }
+
+    private StudioEventEmitter GetLightMoveSound()
+    {
+        return FMODManager.Instance.CreateEventEmitter(
+            FMODManager.Instance.EventDatabase.MovingLight,
+            gameObject,
+            _soundMinDistance, _soundMaxDistance
+        );
+    }
+
+    public void OnLightIdle() => PlayLightMoveSound(0);
+    public void OnLightMoving() => PlayLightMoveSound(1);
+    public void OnLightError() => PlayLightMoveSound(2);
+
+    private void PlayLightMoveSound(int state)
+    {
+        if (!_lightMoveSound.IsPlaying())
+        {
+            _lightMoveSound.Play();
+            FMODManager.Instance.AttachInstance(_lightMoveSound.EventInstance, transform);
+        }
+        _lightMoveSound.SetParameter(EVENT_PARAMETER_STATE, state);
+    }
 }
