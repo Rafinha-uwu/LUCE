b1a3dcd367b683a267c3aa57ea20f4ccc50f0184
Márcio Tavares <marciotavares@ua.pt>
2025-01-21 13:43:18 +0000

    Added logic for Elevator Shake sound
    Created ElevatorShakeAnimationBehaviour.
    FMODEventDatabase asset empty.

---
+++ Assets/Scripts/AnimationBehaviours/ElevatorShakeAnimationBehavior.cs
@@ -0,0 +1,24 @@
+using FMOD.Studio;
+using UnityEngine;
+
+public class ElevatorShakeAnimationBehavior : StateMachineBehaviour
+{
+    private static EventInstance? _shakeSound;
+
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _shakeSound ??= GetShakeSound();
+
+        _shakeSound?.start();
+        if (_shakeSound.HasValue) FMODManager.Instance.AttachInstance(_shakeSound.Value, animator.transform);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _shakeSound?.stop(STOP_MODE.ALLOWFADEOUT);
+    }
+
+
+    private EventInstance? GetShakeSound() => FMODManager.Instance.CreateEventInstance(FMODManager.Instance.EventDatabase.ElevatorShake);
+}
