575a5eae4ead4e5c64b794748319c086a48cc400
Márcio Tavares <marciotavares@ua.pt>
2025-01-21 13:21:16 +0000

    Added logic for Key GroundHit + Unlock sounds
    Created Key script (extends holdableItem).
    GroundHit sound is only played if key is not being held.

--- Assets/Scripts/Interactions/KeyDoor.cs
+++ Assets/Scripts/Interactions/KeyDoor.cs
@@ -1,29 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyDoor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private static readonly string ANIMATOR_PARAMETER = "Open";
+    private Animator _animator;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Awake()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.name == "Key")
-        {
-            this.GetComponent<Animator>().SetBool("Open", true);
-           Destroy(collision.gameObject);
-        }
+        bool hasKey = collision.TryGetComponent(out Key key);
+        if (!hasKey) return;
+
+        _animator.SetBool(ANIMATOR_PARAMETER, true);
+        PlayUnlockSound();
+        Destroy(key.gameObject);
     }
 
 
+    private void PlayUnlockSound()
+    {
+        FMODManager.Instance.PlayOneShotAttached(
+            FMODManager.Instance.EventDatabase.KeyUnlock,
+            gameObject
+        );
+    }
 }
