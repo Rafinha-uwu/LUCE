95bfb36dc681f2a1f75893a91a7ed7aa5208ccc4
Márcio Tavares <marciotavares@ua.pt>
2025-01-23 00:26:27 +0000

    AudioCategory functional with placeholders
    Added logic to create templates to each placeholder bus.
    Added content RectTransform variable in AudioCategory.
    Added logic to set content height accordingly.
    Added logic to set slider values and placeholder values (Open/Apply).
    Changed TemplateAudioSettings Awake to public Initialize method.
    Added logic to set template position according to index.
    Changed ScrollView MovementType + set content variable in scene.

--- Assets/Scripts/Settings/AudioCategory.cs
+++ Assets/Scripts/Settings/AudioCategory.cs
@@ -1,17 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class AudioCategory : SettingsCategory
 {
     [SerializeField] private TemplateAudioSettings _template;
+    [SerializeField] private RectTransform _content;
+    private readonly List<TemplateAudioSettings> _options = new();
+
     [SerializeField] private UnityEngine.UI.Button _applyButton;
+
+
+    private readonly Dictionary<string, int> _testing = new() {
+        { "Master", 100 },
+        { "Music", 75 },
+        { "Ambient", 50 },
+        { "SFX", 25 },
+        { "Voice", 0 }
+    };
 
 
     protected override void Awake()
     {
         base.Awake();
 
+        if (_content == null) throw new System.Exception("Content is not set in the inspector");
         if (_template == null) throw new System.Exception("Template is not set in the inspector");
         if (_applyButton == null) throw new System.Exception("ApplyButton is not set in the inspector");
+
+        _template.Initialize();
     }
 
     private void Start()
@@ -26,6 +42,23 @@ public class AudioCategory : SettingsCategory
         // Get buses from FMODBusDatabase
 
         // Create a text and slider for each bus (according to the template given)
+        int i = 0;
+        foreach (var name in _testing.Keys)
+        {
+            TemplateAudioSettings optionTemplate = Instantiate(_template, _content);
+            
+            optionTemplate.Initialize();
+            optionTemplate.SetLabel(name);
+            optionTemplate.SetToNthPosition(i);
+            optionTemplate.gameObject.SetActive(true);
+
+            _options.Add(optionTemplate);
+            i++;
+        }
+
+        // Set the content height according to the number of options
+        float contentNewHeight = _template.GetHeight() * _testing.Count;
+        _content.sizeDelta = new(_content.sizeDelta.x, contentNewHeight);
     }
 
 
@@ -34,10 +67,24 @@ public class AudioCategory : SettingsCategory
         base.Open();
 
         // Set the values of the sliders
+        for (int i = 0; i < _options.Count; i++)
+        {
+            TemplateAudioSettings option = _options[i];
+            float volume = _testing[option.GetLabel()];
+
+            option.SetSliderValue(Mathf.RoundToInt(volume));
+        }
     }
 
     private void Apply()
     {
         // Set the values of the buses
+        for (int i = 0; i < _options.Count; i++)
+        {
+            TemplateAudioSettings option = _options[i];
+            int volume = option.GetSliderValue();
+
+            _testing[option.GetLabel()] = volume;
+        }
     }
 }
--- Assets/Scripts/Settings/TemplateAudioSettings.cs
+++ Assets/Scripts/Settings/TemplateAudioSettings.cs
@@ -7,11 +7,20 @@ public class TemplateAudioSettings : MonoBehaviour
     [SerializeField] private TMP_Text _labelText;
     [SerializeField] private Slider _slider;
 
+    private RectTransform _rectTransform;
+    private float _initialYPosition;
 
-    private void Awake()
+
+    public void Initialize()
     {
         if (_labelText == null) throw new System.Exception("Label Text is not assigned in the inspector");
         if (_slider == null) throw new System.Exception("Slider is not assigned in the inspector");
+
+        // Get the RectTransform + Set initial Y position
+        bool hasRectTransform = TryGetComponent(out _rectTransform);
+        if (!hasRectTransform) throw new System.Exception("RectTransform is not found in the GameObject");
+
+        _initialYPosition = _rectTransform.anchoredPosition.y;
 
         // Set slider between 0% and 100% without decimals
         _slider.minValue = 0;
@@ -25,4 +34,12 @@ public class TemplateAudioSettings : MonoBehaviour
 
     public void SetLabel(string text) => _labelText.text = text;
     public void SetSliderValue(int value) => _slider.value = value;
+
+
+    public float GetHeight() => _rectTransform.rect.height;
+    public void SetToNthPosition(int n)
+    {
+        float y = _initialYPosition - n * GetHeight();
+        _rectTransform.anchoredPosition = new Vector2(_rectTransform.anchoredPosition.x, y);
+    }
 }
