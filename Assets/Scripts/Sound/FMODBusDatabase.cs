447e8f05cc1c4c4d8510b2bb8534ccfcd0581d77
Márcio Tavares <marciotavares@ua.pt>
2025-01-23 01:44:46 +0000

    Stored Bus volume on PlayerPrefs
    Created methods to Get/Set bus volume in NamedBus.
    Added logic in Initialize method to set Prefs/Bus Volume.

--- Assets/Scripts/Sound/FMODBusDatabase.cs
+++ Assets/Scripts/Sound/FMODBusDatabase.cs
@@ -24,10 +24,45 @@ public class FMODBusDatabase : ScriptableObject
         [field: SerializeField] public string BusPath { get; private set; }
         public Bus Bus { get; private set; }
 
+        private string PlayerPrefsKey => $"{BusName}Volume";
+
+
         public void Initialize()
         {
             // Throws an exception the bus doesn't exist
             Bus = RuntimeManager.GetBus(BusPath);
+
+            // Set the volume to the value stored in PlayerPrefs
+            if (PlayerPrefs.HasKey(PlayerPrefsKey))
+            {
+                float volume = PlayerPrefs.GetFloat(PlayerPrefsKey);
+                Bus.setVolume(volume);
+            }
+            // Set the value stored in PlayerPrefs to the volume
+            else
+            {
+                Bus.getVolume(out float volume);
+                PlayerPrefs.SetFloat(PlayerPrefsKey, volume);
+            }
+        }
+
+
+        public void SetBusVolume(int volume)
+        {
+            float normalizedVolume = Mathf.InverseLerp(0, 100, volume);
+
+            // Store the volume in PlayerPrefs + Set the volume
+            PlayerPrefs.SetFloat(PlayerPrefsKey, normalizedVolume);
+            Bus.setVolume(normalizedVolume);
+        }
+
+        public int GetBusVolume()
+        {
+            // Get the volume from PlayerPrefs
+            float normalizedVolume = PlayerPrefs.GetFloat(PlayerPrefsKey, 1f);
+
+            float volume = Mathf.Lerp(0, 100, normalizedVolume);
+            return Mathf.RoundToInt(volume);
         }
     }
 }
